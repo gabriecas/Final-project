452d9e54a7459d0eb72f936fed054b7958e7e35b
Gabriel <alves.gabriel0013@gmail.com>
2024-03-06 12:30:16 -0300

    Obtendo cursos de uma categoria

--- src/controllers/categoriesController.ts
+++ src/controllers/categoriesController.ts
@@ -1,16 +1,30 @@
 import { Request, Response } from "express";
-import { Category } from "../models";
+import { categoryService } from "../services/categoryService";
+import { getPaginationParams } from "../helpers/getPaginationParams";
 
 export const categoriesController = {
+  // GET/categories
   index: async (req: Request, res: Response) => {
+    const [page, perPage] = getPaginationParams(req.query)
 
     try {
-      const categories = await Category.findAll({
-        attributes: ['id', 'name', 'position'],
-        order: [['position', 'ASC']]
-      })
+      const pagedCategories = await categoryService.findAllPaged(page, perPage)
 
-      return res.json(categories)
+      return res.json(pagedCategories)  
+    } catch (error) {
+      if (error instanceof Error) {
+        return res.status(400).json({ message: error.message })
+      }
+    }
+  },
+  
+  // GET /categories/:id
+  show: async (req:Request, res: Response) => {
+    const { id } = req.params
+
+    try {
+      const category = await categoryService.findByIdWithCourses(id)
+      return res.json(category)
       
     } catch (error) {
       if (error instanceof Error) {
