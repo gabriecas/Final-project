c33ee0c165b50e0bf64f8d8ba11d5eb565e7ac32
Gabriel <alves.gabriel0013@gmail.com>
2024-03-08 17:53:24 -0300

    Autenticação de login com json web token

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { userService } from "../services/userSevice";
+import { jwtService } from "../services/jwtService";
 
 export const authController = {
   // POST /auth/register
@@ -10,7 +11,7 @@ export const authController = {
       const userAlredyExists = await userService.findByEmail(email)
 
       if (userAlredyExists) {
-        throw new Error('Usuário já cadastrado.')
+        throw new Error('Email de usuário já cadastrado.')
       }
 
       const user = await userService.create({
@@ -30,6 +31,34 @@ export const authController = {
         return res.status(400).json({ message: error.message })
       }
     }
+  },
 
+  //POST /auth/login
+  login: async (req: Request, res: Response) => {
+    const { email, password } = req.body
+
+    try {
+      const user = await userService.findByEmail(email)
+
+      if (!user) return res.status(401).json({ message: 'Email não registrado'})
+
+      user.checkPassword(password, (error, same) => {
+        if (error) return res.status(400).json({ message: error.message})
+        if(!same) return res.status(401).json({ message: 'Senha incorreta'})
+
+        const payload = {
+          id: user.id,
+          firstName: user.firstName,
+          email: user.email
+        }
+        const token = jwtService.signToken(payload, '1d')
+
+        return res.json({ authenticated: true, ...payload, token })
+      })
+    } catch (error) {
+      if (error instanceof Error) {
+        return res.status(400).json({ message: error.message })
+      }
+    }
   }
 }
--- src/models/User.ts
+++ src/models/User.ts
@@ -2,6 +2,9 @@ import { DataTypes, Model, Optional } from "sequelize";
 import { sequelize } from "../database";
 import bcrypt from "bcrypt" 
 
+type CheckPasswordCallback = (error?: Error | undefined, same?: boolean) => void
+
+
 export interface User {
   id: number,
   firstName: string,
@@ -15,7 +18,9 @@ export interface User {
 
 export interface UserCreationAttributes extends Optional<User, 'id'> {}
 
-export interface UserInstance extends Model<User, UserCreationAttributes>, User {}
+export interface UserInstance extends Model<User, UserCreationAttributes>, User {
+  checkPassword: (password: string, callbackfn: CheckPasswordCallback) => void
+}
 
 export const User = sequelize.define<UserInstance, User>('User', {
   id: {
@@ -68,3 +73,13 @@ export const User = sequelize.define<UserInstance, User>('User', {
     }
   }
 })
+
+User.prototype.checkPassword = function (password: string, callbackfn: CheckPasswordCallback)  {
+  bcrypt.compare(password, this.password, (error, same) => {
+    if (error) {
+      callbackfn(error)
+    } else {
+      callbackfn(error, same)
+    }
+  })
+}
